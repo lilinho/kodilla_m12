8c21c9e595f0e04e50575b00abf74b8fe3fd26dc
Tomasz Pawelec <30554524+lilinho@users.noreply.github.com>
2017-11-24 11:10:47 +0100

    finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,8 @@
-/*
-TODO
-* variables for api response
-* variables for DOM
-* create DOM
-*/
-
 $(function() {
     
     var url = 'https://restcountries.eu/rest/v1/name/';
     var countryList = $("#countries");
-    console.log(countryList.children()[1].innerText);
+    
     $("#search").click(searchCountries);
     
     function searchCountries() {
@@ -26,28 +19,47 @@ $(function() {
             
         })
     }
-    /*
-    <div class="row">
-    <div class="col-sm-4>FLAG</div>
-    <div class=col-sm-8>COUNTRY NAME</div>
-    </div>
-    
-    
-    */
-    function showCountryList(input) {
-        
-        // check if there were any country searched for
-        
-        if(countryList.children()[0].innerText == "No data") {
-            countryList.empty();
-        }
-        
-        // building DOM
-        var domRow = $('<div>').addClass("row"); // row for Bootstrap Grid
-        var domFlag = $('<div>').addClass("col-sm-4 flag"); // column for flag
-        var domCountryName = $('<div>').addClass("col-sm-8 country-name"); // column for country name
+
+    function showCountryList(response) {
         
         
+        countryList.empty();
         
+        response.forEach(function(data) {
+            
+        var htmlContent = '';
+            
+        htmlContent += '<div class="row">';
+        htmlContent += '<div class="col-sm-4 flag d-flex align-items-center"><img src="https://restcountries.eu/data/' + data.alpha3Code.toLowerCase() + '.svg" alt="Flag of ' + data.name + '"></div>';
+        htmlContent += '<div class="col-sm-8 country-name d-flex align-items-center"><p class="c-name">' + data.name + '</p></div></div>';
+        htmlContent += '<div class="row"><div class="col-sm-12"></div></div>';
+        
+        htmlContent += '<div class="row"><div class="col-sm-6 title"><p>Native name:</p></div>';
+        htmlContent += '<div class="col-sm-6 value"><p>' + data.nativeName + '</p></div></div>';
+            
+        htmlContent += '<div class="row"><div class="col-sm-6 title"><p>Capital:</p></div>';
+        htmlContent += '<div class="col-sm-6 value"><p>' + data.capital + '</p></div></div>';
+            
+        htmlContent += '<div class="row"><div class="col-sm-6 title"><p>Population:</p></div>';
+        htmlContent += '<div class="col-sm-6 value"><p>' + data.population + '</p></div></div>';
+            
+        htmlContent += '<div class="row"><div class="col-sm-6 title"><p>Area:</p></div>';
+        htmlContent += '<div class="col-sm-6 value"><p>' + data.area + ' km<sup>2</sup></p></div></div>';
+        
+        htmlContent += '<div class="row"><div class="col-sm-6 title"><p>Region:</p></div>';
+        htmlContent += '<div class="col-sm-6 value"><p>' + data.region + '</p></div></div>';
+            
+        htmlContent += '<div class="row"><div class="col-sm-6 title"><p>Currency:</p></div>';
+        htmlContent += '<div class="col-sm-6 value"><p>' + data.currencies + '</p></div></div>';
+            
+        htmlContent += '<div class="row"><div class="col-sm-6 title"><p>Internet domain:</p></div>';
+        htmlContent += '<div class="col-sm-6 value"><p>' + data.topLevelDomain[0] + '</p></div></div>';
+            
+        htmlContent += '<div class="row"><div class="col-sm-12"></div></div></div>';
+            
+        countryList.append($('<li>').html(htmlContent));
+        
+        
+    })
     }
 })
